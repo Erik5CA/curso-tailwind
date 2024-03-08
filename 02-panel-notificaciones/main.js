26ed95f6c51bb43d1e47211feeca1e1119493459
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-07 19:27:58 -0600

    terminado panel notificacion

--- 02-panel-notificaciones/main.js
+++ 02-panel-notificaciones/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const markAll = document.querySelector("#mark-all");
+const numNotifi = document.querySelector("#num-notifi");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+markAll.addEventListener("click", () => {
+  const notReadElements = document.querySelectorAll(".not-read");
+  console.log(notReadElements);
+  notReadElements.forEach((notReadElement) => {
+    notReadElement.classList.remove("bg-red-600");
+    notReadElement.classList.remove("not-read");
+  });
 
-setupCounter(document.querySelector('#counter'))
+  const notReadElementsActual = document.querySelectorAll(".not-read");
+  numNotifi.innerText = notReadElementsActual.length;
+});

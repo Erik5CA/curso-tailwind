b02560dbca123cfef4ed6d46b70fd11a0da7fe07
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-08 18:51:14 -0600

    datos de prueba

---
+++ 02-dashboard-dark-mode/src/mooks/data-today.js
@@ -0,0 +1,60 @@
+// Convierte estos datos a un arreglo de objetos con las keys id, action, views y porcentaje
+
+export const dataToday = [
+  {
+    id: 1,
+    action: "Likes",
+    views: 52,
+    porcentaje: 2,
+    name: "Facebook",
+  },
+  {
+    id: 2,
+    action: "Likes",
+    views: 5462,
+    porcentaje: -2257,
+    name: "Facebook",
+  },
+  {
+    id: 3,
+    action: "Profile Views",
+    views: 52000,
+    porcentaje: 1375,
+    name: "Instagram",
+  },
+  {
+    id: 4,
+    action: "Retweets",
+    views: 117,
+    porcentaje: 303,
+    name: "Instagram",
+  },
+  {
+    id: 5,
+    action: "Likes",
+    views: 507,
+    porcentaje: 553,
+    name: "Twitter",
+  },
+  {
+    id: 6,
+    action: "Likes",
+    views: 107,
+    porcentaje: 19,
+    name: "Twitter",
+  },
+  {
+    id: 7,
+    action: "Total Views",
+    views: 1407,
+    porcentaje: -12,
+    name: "YouTube",
+  },
+  {
+    id: 8,
+    action: "Total Views",
+    views: 1407,
+    porcentaje: -12,
+    name: "YouTube",
+  },
+];
---
+++ 02-dashboard-dark-mode/src/mooks/data.js
@@ -0,0 +1,30 @@
+export const dataMedia = [
+  {
+    id: 1,
+    name: "Facebook",
+    username: "@nathanf",
+    followers: 1987,
+    following: 12,
+  },
+  {
+    id: 2,
+    name: "Twitter",
+    username: "@nathanf",
+    followers: 1044,
+    following: 99,
+  },
+  {
+    id: 3,
+    name: "Instagram",
+    username: "@nathanf",
+    followers: 11000,
+    following: 1099,
+  },
+  {
+    id: 4,
+    name: "YouTube",
+    username: "@nathanf",
+    followers: 8239,
+    following: -144,
+  },
+];

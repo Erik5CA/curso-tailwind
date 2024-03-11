9f7df46f8881ee2472c12e400274efebd3ffa931
Erik5CA <erik.castillo.anzures@gmail.com>
2024-03-11 14:17:29 -0600

    grid para md y xl

--- 03-news-homepage-main/src/components/CardNew.jsx
+++ 03-news-homepage-main/src/components/CardNew.jsx
@@ -1,11 +1,11 @@
 import imagen from "../assets/images/image-retro-pcs.jpg";
 export default function CardNew() {
   return (
-    <div className="h-[30%] flex bg-white">
+    <div className="h-[30%] flex bg-white xl:h-full">
       <img className="mr-5" src={imagen} alt="" />
       <div className="flex flex-col  py-1">
         <h2 className="text-4xl font-semibold text-neutral-grayish-blue">02</h2>
-        <h3 className="text-neutral-very-dark-blue text-2xl font-bold">
+        <h3 className="text-neutral-very-dark-blue text-2xl font-bold hover:text-primary-soft-red hover:cursor-pointer">
           Reviving Retro PCs
         </h3>
         <p className="text-neutral-grayish-blue text-base">
